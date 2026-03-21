e08d4f4876ff09b8aa24c224548204b8ca0f5ba5
Maksim Samoilov <git@nitive.me>
2026-03-21 17:05:11 +0400

    Fix not reading env KEYBOARD_DEVICE when started

--- kmonad-start.sh
+++ kmonad-start.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
-/usr/bin/kmonad /home/nitive/Develop/keyboard/kmonad.kbd
+tmp_dir="$(mktemp --directory)"
+envsubst < /home/nitive/Develop/keyboard/kmonad.kbd > "$tmp_dir/kmonad.kbd"
+
+/usr/bin/kmonad "$tmp_dir/kmonad.kbd"
